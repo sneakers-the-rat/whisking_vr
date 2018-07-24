264c5185e84bcd02c00d233c8d83d77a8ec00f34
ClaireRusch <ruschc2@uw.edu>
2018-07-24 18:02:18 -0400

    Who'sWho?

--- MouseReader.py
+++ MouseReader.py
@@ -5,8 +5,8 @@ from inputs import devices
 
 # Handle for getting mouse events
 #mouse = devices.mice[0]
-m = devices.mice[0]
-m2 = devices.mice[1]
+m = devices.mice[1]
+#m2 = devices.mice[1]
 #mouse2 = devices.mice[1]
 #mice = {'mouse_1':mouse}
 #        'mouse_2':mouse2}
