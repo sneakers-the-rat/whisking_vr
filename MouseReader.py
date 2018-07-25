3723f2b7d13469af33bfb59e09ee057a02b42f21
ClaireRusch <ruschc2@uw.edu>
2018-07-25 00:45:02 -0400

    Weeeeeeeeeeee

--- MouseReader.py
+++ MouseReader.py
@@ -1,64 +1,10 @@
 from inputs import devices
 from threading import Thread
-from Queue import Queue
+from Queue import Queue, Empty
 import pandas as pd
 
 import numpy as np
 import time
-
-# Handle for getting mouse events
-#mouse = devices.mice[0]
-#m = devices.mice[0]
-#m2 = devices.mice[1]
-#mouse2 = devices.mice[1]
-#mice = {'mouse_1':m}
- #       'mouse_2':m2}
-
-####################################
-# Run the loop, monitor for movements.
-#movements = []
-#while 1:
- #   for name, m in mice.items():
-  #  events = m.read()
-   # for event in events:
-        #if event.code == 'REL_X':
-        #print('{} - moved {} X: {}'.format(event.timestamp, event.code, event.state))
-        #elif event.code == 'REL_Y':
-        #    print('{} - moved {} Y: {}'.format(event.timestamp, name, event.state))
-    #data_temp[0] = event.timestamp
-    #data_temp[1] = (event[0].code+
-    #data_temp[2] =
-    #data_temp[3] =
-    
-###################### Let's try with thread ###########
-#import threading
-#from inputs import devices
-#M1 = devices.mice[0]
-#M2 = devices.mice[1]
-
-#class Ball():
- #   def mouse(m):
-  #      while True:
-   #         print 'I read this line at least'
-    #        event = m.read()
-     #       print('{} - moved {} X: {}'.format(event.timestamp, event.code, event.state))
-
-#def mouse2():
- #   while True:
-  #      print 'I read this line at least'
-   #     event2 = m2.read()
-    #    print('{} - moved {} X: {}'.format(event2.timestamp, event2.code, event2.state))
-
-#mouse_1 = threading.Thread(name='mouse1', target=mouse, args = (M1,))
-#mouse_2 = threading.Thread(name='mouse2', target=mouse, args = (M2,))
-
-#mouse_1.start()
-#mouse_2.start()
-
-
-
-
-##### Let's have fun!!
 # Handle for getting mouse events
 #mouse = devices.mice[0]
 m = devices.mice[1]
@@ -123,70 +69,7 @@ while True:
     BdX = (x1+x2)/(2*COS45)
     BdY = (y1-y2)/(2*COS45)
     BdTheta = -1*(x1+x2)/(ballDiameter)
-    # Coordinate system with +x (forward), +y (right), +theta (clockwise)
-    #DeltaPos = np.array([BdX, BdY, BdTheta]) 
-            
-    #lastX = x1
-    #lastY = y1
-    #lastTheta = BdTheta
-            
-    #BX = lastX + (BdX*np.cos(lastTheta)-BdY*np.sin(lastTheta))
-    #BY = lastY + (BdX*np.sin(lastTheta)+BdY*np.cos(lastTheta))
-    #BTheta = lastTheta + BdTheta
-            
-    #return np.hstack((time1, time2, BdX, BdY, BdTheta, BX, BY, BTheta))
     B = time.time()
     #print BX
     print('cycle time: {}, dx: {}, dy: {}, theta: {}'.format(B-A, BdX, BdY, BdTheta))
     time.sleep(0.005)
-    
-    
-        
-##
-##while 1:
-##    A = time.time()
-##    events = m.read()
-##    try:
-##        for event in events:
-##            if event.code == 'REL_X':
-##                #print('{} - moved {} X: {}'.format(event.timestamp, event.code, event.state))
-##                x1 = event.state
-##                t = event.timestamp
-##                #print x1
-##            elif event.code == 'REL_Y':
-##                #print('{} - moved {} Y: {}'.format(event.timestamp, name, event.state))
-##                y1 = event.state
-##                #print y1
-##    except:
-##        pass
-##    events2 = m2.read()
-##    try:
-##        for event2 in events2:
-##            if event2.code == 'REL_X':
-##            #print('{} - moved {} X: {}'.format(event.timestamp, event.code, event.state))
-##                x2 = event2.state
-##                t2 = event2.timestamp
-##        elif event.code == 'REL_Y':
-##            #print('{} - moved {} Y: {}'.format(event.timestamp, name, event.state))
-##                y2 = event2.state
-##                t2 = event2.timestamp
-##    except:
-##        pass 
-##    BdX = (x1+x2)/(2*COS45)
-##    BdY = (y1-y2)/(2*COS45)
-##    BdTheta = -1*(x1+x2)/(ballDiameter)
-##    # Coordinate system with +x (forward), +y (right), +theta (clockwise)
-##    DeltaPos = np.array([BdX, BdY, BdTheta]) 
-##            
-##    lastX = x1
-##    lastY = y1
-##    lastTheta = BdTheta
-##            
-##    BX = lastX + (BdX*np.cos(lastTheta)-BdY*np.sin(lastTheta))
-##    BY = lastY + (BdX*np.sin(lastTheta)+BdY*np.cos(lastTheta))
-##    BTheta = lastTheta + BdTheta
-##            
-##    #return np.hstack((time1, time2, BdX, BdY, BdTheta, BX, BY, BTheta))
-##    B = time.time()
-##    #print BX
-##    print B-A
