2dcc3aef5f6cf79206c56bab1b12f49790a6da03
ClaireRusch <ruschc2@uw.edu>
2018-07-24 20:10:53 -0400

    Thread_Trial1

--- MouseReader.py
+++ MouseReader.py
@@ -5,25 +5,46 @@ from inputs import devices
 
 # Handle for getting mouse events
 #mouse = devices.mice[0]
-m = devices.mice[1]
+#m = devices.mice[0]
 #m2 = devices.mice[1]
 #mouse2 = devices.mice[1]
-#mice = {'mouse_1':mouse}
-#        'mouse_2':mouse2}
+#mice = {'mouse_1':m}
+ #       'mouse_2':m2}
 
 ####################################
 # Run the loop, monitor for movements.
-movements = []
-while 1:
-    #for name, m in mice.items():
-    events = m.read()
-    for event in events:
+#movements = []
+#while 1:
+ #   for name, m in mice.items():
+  #  events = m.read()
+   # for event in events:
         #if event.code == 'REL_X':
-        print('{} - moved {} X: {}'.format(event.timestamp, event.code, event.state))
+        #print('{} - moved {} X: {}'.format(event.timestamp, event.code, event.state))
         #elif event.code == 'REL_Y':
         #    print('{} - moved {} Y: {}'.format(event.timestamp, name, event.state))
-    #events_mouse = m2.read()
-    #for event2 in events_mouse:
-     #   print('{} - moved {} X: {}'.format(event2.timestamp, event2.code, event2.state))
+    #data_temp[0] = event.timestamp
+    #data_temp[1] = (event[0].code+
+    #data_temp[2] =
+    #data_temp[3] =
+    
+###################### Let's try with thread ###########
+import threading
+from inputs import devices
+m = device.mice[0]
+m2 = device.mice[1]
 
+
+def mouse1():
+    events = m.read()
+    print('{} - moved {} X: {}'.format(event.timestamp, event.code, event.state))
+
+def mouse2():
+    events = m2.read()
+    print('{} - moved {} X: {}'.format(event.timestamp, event.code, event.state))
+
+mouse_1 = threading.Thread(name='mouse1', target=mouse1)
+mouse_2 = threading.Thread(name='mouse2', target=mouse2)
+
+mouse_1.start()
+mouse_2.start()
 
