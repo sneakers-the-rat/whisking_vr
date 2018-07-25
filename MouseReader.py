3bcc933be9aa3d39729bb3c6680ca69da1985a89
ClaireRusch <ruschc2@uw.edu>
2018-07-25 14:43:28 -0400

    floats & restoring reporting real numbers

--- MouseReader.py
+++ MouseReader.py
@@ -14,7 +14,7 @@ import time
 
 data = np.zeros((1,8))
 COS45 = np.cos(np.pi/4)
-ballDiameter = 150  #more or less
+ballDiameter = 150.  #more or less
 x1 = 0
 y1 = 0
 y2 = 0
@@ -33,9 +33,9 @@ def poll_mouse(mouse, out_queue):
             # we stash the timestamp, then compare each next event to see if it matches
             # if so, we assume we got the other one and send the frame
             # if not, we assign the missing value zero and send it.
- #           frame['x'] = 1
- #           frame['y'] = 1
-            out_queue.put_nowait(frame)
+            #frame['x'] = 1
+            #frame['y'] = 1
+            #out_queue.put_nowait(frame)
             frame = {}
             if len(frame) == 0:
                 if event.code == 'REL_X':
@@ -65,7 +65,7 @@ def poll_mouse(mouse, out_queue):
                         frame['x'] = 0.
                         out.queue.put_nowait(frame)
                         frame = {}
-##                        
+                        
                     
 
 mice = {'mouse_0':devices.mice[0],
@@ -106,10 +106,10 @@ while True:
     
     BdX = (x1+x2)/(2*COS45)
     BdY = (y1-y2)/(2*COS45)
-    BdTheta = -1*(x1+x2)/(ballDiameter)
+    BdTheta = -1.*(x1+x2)/(ballDiameter)
     
     B = time.time()
     #print BX
     #print('cycle time: {}, dx: {}, dy: {}, theta: {}'.format(B-A, BdX, BdY, BdTheta))
     print('cycle time: {}, x1: {}, y1: {}, x2 : {}, y2 : {}'.format(B-A, x1, y1, x2, y2))
-    time.sleep(2)
+    time.sleep(0.5)
